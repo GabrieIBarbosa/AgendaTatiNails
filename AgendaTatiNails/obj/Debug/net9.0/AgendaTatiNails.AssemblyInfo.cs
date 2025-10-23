9fed55c29a44b2191a82218d96b976b9210b4c93
Gabriel Barbosa <gabrielofcbarbosa22@gmail.com>
2025-10-23 15:58:26 -0300

    Adiciona .gitignore para ignorar pastas bin e obj

--- AgendaTatiNails/obj/Debug/net9.0/AgendaTatiNails.AssemblyInfo.cs
+++ AgendaTatiNails/obj/Debug/net9.0/AgendaTatiNails.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendaTatiNails")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a21616e499fc65365b93f0bf31219dc989ed70")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendaTatiNails")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendaTatiNails")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
